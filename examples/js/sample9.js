4c075c43b814226af7bf5775b1f8ac09c8063622
mtoshi <mtoshi.g@gmail.com>
2013-05-28 14:18:20 +0900

    Changed sample

---
+++ examples/js/sample9.js
@@ -0,0 +1,13 @@
+$(function(){
+   $("#sample9").linkDraw({
+     "configPath": "configs/sample9_config.json", 
+     "positionPath": "positions/sample9_position.json", 
+     "positionWriter": "write.cgi",
+     "positionSave": false,
+     //"zoom": false,
+     //"drag": false,
+     "width": 400,
+     "height": 250,
+     "interval":0 
+   });
+});
