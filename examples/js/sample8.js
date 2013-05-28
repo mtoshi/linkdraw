7aa20b23b0d18fe2b41d2389dc8fb92cf98179f3
mtoshi <mtoshi.g@gmail.com>
2013-05-28 11:40:28 +0900

    Changed sample data

---
+++ examples/js/sample8.js
@@ -0,0 +1,13 @@
+$(function(){
+   $("#sample8").linkDraw({
+     "configPath": "configs/sample8_config.json", 
+     "positionPath": "positions/sample8_position.json", 
+     "positionWriter": "write.cgi",
+     "positionSave": false,
+     //"zoom": false,
+     //"drag": false,
+     "width": 200,
+     "height": 200,
+     "interval":0 
+   });
+});
