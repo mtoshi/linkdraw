937fcafc71bde091238a99b7e974b41c52ae7168
mtoshi <mtoshi.g@gmail.com>
2013-05-29 14:41:17 +0900

    Added sample13

---
+++ examples/js/sample13.js
@@ -0,0 +1,13 @@
+$(function(){
+   $("#sample13").linkDraw({
+     "configPath": "configs/sample13_config.json", 
+     "positionPath": "positions/sample13_position.json", 
+     "positionWriter": "write.cgi",
+     "positionSave": false,
+     //"zoom": false,
+     //"drag": false,
+     "width": 800,
+     "height": 600,
+     "interval":0 
+   });
+});
