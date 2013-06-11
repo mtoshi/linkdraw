2538f21c87e32689fb0632f747bfbe272a31a546
mtoshi <mtoshi.g@gmail.com>
2013-06-11 19:39:03 +0900

    Changed tree

--- utils/setup.sh
+++ utils/setup.sh
@@ -5,6 +5,7 @@ cp -a src/* ${1}/
 cp -a examples/positions/* ${1}/positions/
 cp -a examples/configs/* ${1}/configs/
 cp -a examples/js/* ${1}/js/
+cp -a examples/css/* ${1}/css/
 cp -a examples/html/* ${1}/
 chmod 666 ${1}/positions/*.json
 chmod 666 ${1}/configs/*.json
