39c2709d0c91b2a0785e85617f29e990ae1c8907
mtoshi <mtoshi.g@gmail.com>
2013-06-11 21:31:29 +0900

    Changed setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 test -d $1 || mkdir $1
+
 cp -a src/* ${1}/
-cp -a examples/positions/* ${1}/positions/
-cp -a examples/configs/* ${1}/configs/
-cp -a examples/js/* ${1}/js/
-cp -a examples/css/* ${1}/css/
 cp -a examples/html/* ${1}/
+cp -a examples/positions ${1}/
+cp -a examples/configs ${1}/
+cp -a examples/js ${1}/
+cp -a examples/css ${1}/
 chmod 666 ${1}/positions/*.json
 chmod 666 ${1}/configs/*.json
 chmod 755 ${1}/js/*.js
