8bec5da1eb7a7bd9ffbdd46f4c98e25d83486925
mtoshi <mtoshi.g@gmail.com>
2013-06-25 19:36:16 +0900

    Changed setup.sh

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 test -d $1 || mkdir $1
-dir=$(cd $(dirname $0);pwd)
-cp -a ${dir}/../src/* ${1}/
-cp -a ${dir}/../examples/html/* ${1}/
-cp -a ${dir}/../examples/positions ${1}/
-cp -a ${dir}/../examples/configs ${1}/
-cp -a ${dir}/../examples/js ${1}/
-cp -a ${dir}/../examples/css ${1}/
+cdir=$(cd $(dirname $0);pwd)
+cp -a ${cdir}/../src/* ${1}/
+cp -a ${cdir}/../examples/html/* ${1}/
+cp -a ${cdir}/../examples/positions ${1}/
+cp -a ${cdir}/../examples/configs ${1}/
+cp -a ${cdir}/../examples/js ${1}/
+cp -a ${cdir}/../examples/css ${1}/
 chmod 666 ${1}/positions/*.json
 chmod 666 ${1}/configs/*.json
 chmod 755 ${1}/js/*.js
